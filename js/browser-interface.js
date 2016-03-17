693b6a5421224929bb974db49fcb001f8d52239d
michael dada <mail.michaeldada@gmail.com>
2016-03-17 10:48:43 -0700

    reformat app files

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,15 +1,9 @@
-var makeRequest = require('./../js/search.js').makeRequest;
-var makeRequest2 = require('./../js/search.js').makeRequest2;
 
-$("#search-button").submit(function(event){
-  event.preventDefault();
-  var keyWordsearch = function(){
+var keyWordsearch = require('./../js/search.js').keyWordsearch;
 
-    gapi.client.setApiKey('AIzaSyCvjqLF84b-96a8p5KeWKutbiqy3f9SsYA');
-    gapi.client.load('youtube', 'v3', function() {
-    makeRequest();
-    makeRequest2();
+$(document).ready(function() {
+  $('#search-button').click(function() {
+    keyWordsearch();
 
   });
 });
-};
--- js/search.js
+++ js/search.js
@@ -1,42 +1,63 @@
-exports.makeRequest = function() {
-        var q = $('#song').val() + $('#artist').val();
-        console.log(q);
-        var request = gapi.client.youtube.search.list({
-         q:q,
-         part:"id",
-         type:"video",
-         fields:"items/id/videoId"
-        });
-        request.execute(function(response) {
 
-        var str = JSON.stringify(response.items[0].id.videoId);
-        str = str.replace(/[^\w\s]/gi, '')
-        console.log(str);
-        var url = "https://www.youtube.com/embed/" + str+ "?autoplay=1";
-        console.log(url);
-        $('#myIframe').attr('src', url)
+exports.keyWordsearch = function(){
+        gapi.client.setApiKey('AIzaSyCvjqLF84b-96a8p5KeWKutbiqy3f9SsYA');
+        gapi.client.load('youtube', 'v3', function() {
 
-        console.log(str);
-        });
-}
+          function makeRequest() {
+            var q = $('#song').val() + $('#artist').val();
+            // console.log(q);
+            var request = gapi.client.youtube.search.list({
+             q:q,
+             part:"id",
+             type:"video",
+             fields:"items/id/videoId"
+            });
+            request.execute(function(response) {
+      // debugger;
+            var str = JSON.stringify(response.items[0].id.videoId);
+            str = str.replace(/['"]+/g, '');
+            console.log(str.replace(/['"]+/g, ''));
+            console.log(str);
+            var url = "https://www.youtube.com/embed/" + str + "?autoplay=1";
+            console.log(url);
+            $('#myIframe').attr('src', url)
 
-exports.makeRequest2 = function() {
-        var q = $('#song').val() + $('#artist').val() + "parody";
-        console.log(q);
-        var request = gapi.client.youtube.search.list({
-         q:q,
-         part:"id",
-         type:"video",
-         fields:"items/id/videoId"
-        });
-        request.execute(function(response) {
-          // debugger;
-        var str = JSON.stringify(response.items[0].id.videoId);
-        str = str.replace(/[^\w\s]/gi, '')
-        console.log(str);
-        var url = "https://www.youtube.com/embed/" + str + "?autoplay=1";
-        console.log(url);
-        $('#parodyIframe').attr('src', url)
+
+
+
+                    console.log(str);
+                  });
+          }
+
+          function makeRequest2() {
+                  var q = $('#song').val() + $('#artist').val()+"parody";
+                  console.log(q);
+                  var request = gapi.client.youtube.search.list({
+                   q:q,
+                   part:"id",
+                   type:"video",
+                   fields:"items/id/videoId"
+                  });
+                  request.execute(function(response) {
+            // debugger;
+                  var str = JSON.stringify(response.items[0].id.videoId);
+                  str = str.replace(/['"]+/g, '');
+                  console.log(str.replace(/['"]+/g, ''));
+                  console.log(str);
+                  var url = "https://www.youtube.com/embed/" + str + "?autoplay=1";
+                  console.log(url);
+                  $('#parodyIframe').attr('src', url)
+
+
+
+
+                  console.log(str);
+          });
+          }
+
+
+        makeRequest();
+        makeRequest2();
 
         });
 }
