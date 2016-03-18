2afca732189216e506cc6c0006645e8761999470
Michael Dada <mail.michaeldada@gmail.com>
2016-03-18 13:48:21 -0700

    modify api request to only return list of public repos from user

--- js/getRepos.js
+++ js/getRepos.js
@@ -4,9 +4,10 @@ exports.getRepos = function(){
   var user = $('#username').val();
   console.log(user);
   $('#username').val("");
-  $.get('https://api.github.com/users/'+user+'?access_token=' + apiKey).then(function(response){
-    console.log(response);
+  $.get('https://api.github.com/users/'+user+'/repos'+'?access_token='+apiKey).then(function(response){
+      console.log(response)
   }).fail(function(error){
+
     console.log(error.responseJSON.message);
   });
 };
